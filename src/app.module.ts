d31b28d6839f4180fd9e53e00404603babbcc453
ahb.62 <ahb.62@protonmail.com>
2023-01-28 23:15:26 -0400

    Connection to database fixed & modules, services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { TasksController } from './tasks/tasks.controller';
+// import { TasksController } from './tasks/tasks.controller';
+// import { TasksModule } from './tasks/tasks.module';
+// import { TasksService } from './tasks/tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 import { TasksModule } from './tasks/tasks.module';
-
 @Module({
-  imports: [DatabaseModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'tasks',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+    TasksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
-import { DatabaseModule } from 'src/database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { Task } from 'src/entities/task.entity';
 
